4d164639ad6a95cb94254a833023cc6c533e604b
Z1A1 <105920041+z1a1com@users.noreply.github.com>
2022-05-20 11:43:22 +0100

    Add files via upload

---
+++ precache-manifest.cf16afdbdc9ff1fd4daaf7abcaaba1cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcf77555e9ceded8b421",
+    "url": "/static/css/main.a209920f.chunk.css"
+  },
+  {
+    "revision": "dcf77555e9ceded8b421",
+    "url": "/static/js/main.dcf77555.chunk.js"
+  },
+  {
+    "revision": "877ddeaf8032078f6663",
+    "url": "/static/js/1.877ddeaf.chunk.js"
+  },
+  {
+    "revision": "a0d76e7833493d411a4f",
+    "url": "/static/css/2.deaa4e7a.chunk.css"
+  },
+  {
+    "revision": "a0d76e7833493d411a4f",
+    "url": "/static/js/2.a0d76e78.chunk.js"
+  },
+  {
+    "revision": "178d12507e2f6a393eb1",
+    "url": "/static/js/runtime~main.178d1250.js"
+  },
+  {
+    "revision": "629c560dc3669c16815b7125db94f8ee",
+    "url": "/static/media/z1a1-800-600.629c560d.png"
+  },
+  {
+    "revision": "87d3647dd02ed170952170537d8d04c9",
+    "url": "/static/media/error-page-1920-1240.87d3647d.webp"
+  },
+  {
+    "revision": "32673bb642d382103ecb55f4592615ad",
+    "url": "/index.html"
+  }
+];
